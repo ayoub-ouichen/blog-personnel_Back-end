7cd7733a8b7f37156b28f1b2f64cbd93b50b33c7
Ouichen Ayoub <ayoub.wichen705@gmail.com>
2023-08-23 19:28:46 +0100

    In the way to present a poste 2

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const posteRoute = require('./routes/poste');
 
 app.use(cors({credentials: true, origin: true}));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cookieParser())
+app.use(express.json({limit: '50mb'}));
 app.use('/user', userRoute);
 app.use('/poste', posteRoute);
 
--- routes/poste.js
+++ routes/poste.js
@@ -3,6 +3,13 @@ const connection = require('../connection');
 const router = express.Router();
 const { authentifierToken } = require('../utils/authorization');
 
+router.get('/lire-poste', authentifierToken, (req, res) => {
+  connection.query('SELECT * FROM poste', (err, result) => {
+    if (err) console.log(err);
+    else res.send(result.recordset[0]);
+  });
+});
+
 router.post('/nouveau-poste', authentifierToken, async (req,res)=>{
   
   const poste = req.body;
@@ -11,8 +18,9 @@ router.post('/nouveau-poste', authentifierToken, async (req,res)=>{
     let request = new connection.Request();
     request.input('param1', connection.VarChar, poste.p_titre);
     request.input('param2', connection.VarChar, poste.p_contenu);
+    request.input('param3', connection.Int, poste.p_lire_temps);
 
-    let result = await request.query('INSERT INTO poste (p_titre,p_contenu,p_date_creation) values (@param1,@param2,getdate());');
+    let result = await request.query('INSERT INTO poste (p_titre,p_contenu,p_date_creation,p_lire_temps) values (@param1,@param2,getdate(),@param3);');
     
     if (result.rowsAffected == 1) {
         return res.status(200).json({ success: true, message: 'Poste ajouté avec succès' });
